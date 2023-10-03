ab80194b6a27b296d0415361a4001d2eb56573ae
pytbyte <heretolearn1@gmail.com>
2023-10-03 04:01:49 +0300

    load ballancer puppet add custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# add a custom HTTP header X-Served-By
+
+exec {'get_updates':
+  command => '/usr/bin/apt-get update',
+}
+
+package {'nginx':
+  ensure  => 'installed',
+  require => Exec['get_updates'],
+}
+
+$custom_header = "         add_header X-Served-By \"${::hostname}\";"
+
+exec {'add_custom_header':
+command  => "echo '${custom_header}' | sudo sed -i '50r /dev/stdin' /etc/nginx/sites-enabled/default",
+  provider => 'shell',
+}
+
+service {'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
+}
