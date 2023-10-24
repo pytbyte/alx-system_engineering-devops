6c5a7fe6f870d9d81ec364cf5b8cc16dddb2012e
pytbyte <heretolearn1@gmail.com>
2023-10-24 20:20:40 +0300

    Web API 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python3
-"""fetch ifo by employee ID."""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 
-def fetch_json(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-    return response.json()
-
-
 def todo_progress(employee_id):
+    # Define the API endpoint
     base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos'
 
-    user_data = fetch_json(f'{base_url}/users/{employee_id}')
-    if user_data is None:
+    # Fetch user data
+    users = requests.get(user_url)
+
+    if users.status_code != 200:
         print(f"Error: Could not retrieve user data for ID {employee_id}")
         return
 
+    user_data = users.json()
     user_name = user_data.get('name')
-    if not user_name:
+
+    if user_name is None:
         print(f"Error: User name not found for ID {employee_id}")
         return
 
-    todo_data = fetch_json(f'{base_url}/todos?userId={employee_id}')
-    if not todo_data:
+    # Fetch todos data for a specific user using the query parameter
+    params = {"userId": employee_id}
+    tasks = requests.get(todos_url, params=params)
+
+    if tasks.status_code != 200:
+        print(f"Error: Could not retrieve data for user ID {employee_id}")
+        return
+
+    pending_tasks = tasks.json()
+
+    if not pending_tasks:
         print(f"No TODO list data found for user ID {employee_id}")
         return
 
-    total_tasks = len(todo_data)
-    done_data = sum(1 for todo in todo_data if todo.get('completed'))
+    total_tasks = len(pending_tasks)
+    completed_tasks = sum(1 for todo in pending_tasks if todo.get('completed'))
 
-    print(f"Employee {user_name} is done with tasks
-          ({done_data}/{total_tasks})")
+    print(f"Employee {user_name} is done with tasks"
+          f"({completed_tasks}/{total_tasks}):")
 
-    for todo in todo_data:
+    for todo in pending_tasks:
         if todo.get('completed'):
-            print(f"\t{todo.get('title')}")
+            print(f"	 {todo.get('title')}")
 
 
 if __name__ == "__main__":
