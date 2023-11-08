d2763e6a41a0b260f39bb2f91ad904780d66e48b
pytbyte <heretolearn1@gmail.com>
2023-11-08 16:12:07 +0300

    Recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""
+Recursive function that queries Reddit API .
+"""
 import requests
 
 
-def count_words_in_hot_posts(subreddit,
-                             words_to_count,
-                             after=None,
-                             word_counts={}):
-    """Prints counts of specified words of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        words_to_count (list): The list of words to search for in post titles.
-        after (str): The parameter for the next page of the API results.
-        word_counts (dict): The parameter to store results matched thus far.
-    """
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    # Set a custom User-Agent and disable following redirects
     headers = {'User-Agent': 'RedditDataAnalyzer/1.0 (ALX Africa)'}
     params = {'limit': 100}  # Limit the number of posts to 100 (maximum)
 
@@ -30,38 +21,16 @@ def count_words_in_hot_posts(subreddit,
     if response.status_code == 200:
         data = response.json()
 
-        # Extract and parse the titles of the posts
         for post in data.get('data', {}).get('children', []):
-            title = post.get('data', {}).get('title', '').lower()
-            words_in_title = title.split()
-
-            # Count the occurrences of keywords in the title
-            for word in words_to_count:
-                if word.lower() in words_in_title:
-                    times = words_in_title.count(word.lower())
-                    if word_counts.get(word.lower()) is None:
-                        word_counts[word.lower()] = times
-                    else:
-                        word_counts[word.lower()] += times
+            title = post.get('data', {}).get('title', '')
+            hot_list.append(title)
 
         # Check if there are more pages (pagination) and continue the recursion
         after = data.get('data', {}).get('after')
         if after:
-            return count_words_in_hot_posts(subreddit,
-                                            words_to_count,
-                                            after,
-                                            word_counts)
+            return recurse(subreddit, hot_list, after)
 
-        if len(word_counts) == 0:
-            return
-
-        # If no more pages, print the sorted results
-        sorted_word_counts = sorted(word_counts.items(),
-                                    key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_word_counts:
-            print("{}: {}".format(word, count))
-
-    elif response.status_code == 404:
-        return
+        # If no more pages, return the hot_list
+        return hot_list
     else:
-        return
+        return None
