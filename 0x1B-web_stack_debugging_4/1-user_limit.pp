5344d3e3d1c0f955dbbad646e37d0e32797a73d9
pytbyte <heretolearn1@gmail.com>
2023-11-17 01:46:32 +0300

    webstack debug4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
-# increase max open files for holberton user
-exec { 'replace_hard_limit':
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
+# scale open files for holberton user
+exec { 'set_holberton_limit':
+  command  => 'sed -i "s/holberton init nofile 5/holberton init nofile 5000/" /etc/security/limits.conf',
   provider => 'shell'
 }
-exec { 'replace_soft_limit':
-  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+exec { 'set_holberton_limit_1':
+  command  => 'sed -i "s/holberton latter nofile 4/holberton latter nofile 4000/" /etc/security/limits.conf',
   provider => 'shell'
 }
